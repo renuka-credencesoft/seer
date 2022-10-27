3ebd0f21071b4c90a1f8542fdd8f09470e9659ce
inzamam <inzamam@credencesoft.in>
2022-10-27 17:45:21 +0530

    Build solution page with responsiveness

---
+++ src/components/SolutionVideo/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './index.css';
+import VideoSection from '../../assets/images/VideoSection.png';
+import calendar1 from '../../assets/images/365.png';
+import calendar2 from '../../assets/images/7.png';
+
+const SolutionVideo = () => {
+  return (
+    <div className='SolutionVideoContainer'>
+        <div className="SolutionVideoLeftSection">
+            <img src={VideoSection} alt="Video section" />
+        </div>
+        <div className="SolutionVideoRightSection">
+            <div className="SolutionVideoRightSectionCard">
+                <div className="SolutionVideoRightSectionCardTopContent">
+                    <div className="SolutionVideoRightSectionCardImg">
+                        <img src={calendar2} alt="365" />
+                    </div>
+                    <p>Weekly Demand Forecast</p>
+                </div>
+                <div className="SolutionVideoRightSectionCardBottomContent">
+                    <p>Optimize stock levels in near real-time to adapt to dynamic changes in consumption. Respond to proposed promotions, subtle shifts in demand, and known events.</p>
+                </div>
+
+            </div>
+            <div className="SolutionVideoRightSectionCard">
+                <div className="SolutionVideoRightSectionCardTopContent">
+                    <div className="SolutionVideoRightSectionCardImg">
+                        <img src={calendar1} alt="365" />
+                    </div>
+                    <p>Annual Demand Forecast</p>
+                </div>
+                <div className="SolutionVideoRightSectionCardBottomContent">
+                    <p>Optimize stock levels in near real-time to adapt to dynamic changes in consumption. Respond to proposed promotions, subtle shifts in demand, and known events.</p>
+                </div>
+
+            </div>
+            <div className="SolutionVideoRightSectionCard">
+                <div className="SolutionVideoRightSectionCardTopContent">
+                    <div className="SolutionVideoRightSectionCardImg">
+                        <img src={calendar1} alt="365" />
+                    </div>
+                    <p>Simulation</p>
+                </div>
+                <div className="SolutionVideoRightSectionCardBottomContent">
+                    <p>Optimize stock levels in near real-time to adapt to dynamic changes in consumption. Respond to proposed promotions, subtle shifts in demand, and known events.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SolutionVideo
--- src/pages/Solution/index.js
+++ src/pages/Solution/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ClientsCarousel from '../../components/ClientsCarousel';
+import SolutionVideo from '../../components/SolutionVideo';
 import VideoSection from '../../components/VideoSection';
 import './index.css';
 
 const Solution = () => {
   return (
     <div className='SolutionContainer'>
+        <SolutionVideo />
         <VideoSection />
         <ClientsCarousel />
     </div>
