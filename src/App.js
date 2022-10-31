deb44094531cd7c422b2a13e7c6da965fdb38df1
rekha-credencesoft <rekha@credencesoft.in>
2022-10-31 11:14:48 +0530

    trial

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
-import { useState } from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import ResponsiveNav from './components/ResponsiveNav';
-import Home from './pages/Home';
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import ResponsiveNav from "./components/ResponsiveNav";
+import Home from "./pages/Home";
 import About from "./pages/About";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Footer from './components/Footer';
-import Solution from './pages/Solution';
-import Contact from './pages/Contact';
-import Blog from './pages/Blog';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Solution from "./pages/Solution";
+import Contact from "./pages/Contact";
+import Blog from "./pages/Blog";
+import ReactDOM from "react-dom/client";
 
-function App() {
+export default function App() {
   const [showResponsiveNav, setShowResponsiveNav] = useState(false);
 
   return (
-      // <Router>
-        <div className="AppContainer">
-          <Home />
-          <Navbar setShowResponsiveNav={setShowResponsiveNav} showResponsiveNav={showResponsiveNav} />
-          <ResponsiveNav setShowResponsiveNav={setShowResponsiveNav} showResponsiveNav={showResponsiveNav} />
-          {/* <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/about' element={<About />} />
-            <Route exact path='/solutions' element={<Solution />} />
-            <Route exact path='/contact' element={<Contact />} />
-            <Route exact path='/blog' element={<Blog />} />
-          </Routes> */}
-          <Footer />
-        </div>
-      // </Router>
+    <BrowserRouter>
+      <div className="AppContainer">
+        <Navbar
+          setShowResponsiveNav={setShowResponsiveNav}
+          showResponsiveNav={showResponsiveNav}
+        />
+        <ResponsiveNav
+          setShowResponsiveNav={setShowResponsiveNav}
+          showResponsiveNav={showResponsiveNav}
+        />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/solutions" element={<Solution />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/blog" element={<Blog />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
