d73ac6597ff5119192f7a92ac22cede852c68493
rekha-credencesoft <rekha@credencesoft.in>
2022-10-31 10:58:20 +0530

    trial

--- src/App.js
+++ src/App.js
@@ -18,20 +18,21 @@ function App() {
   const [showResponsiveNav, setShowResponsiveNav] = useState(false);
 
   return (
-      <Router>
+      // <Router>
         <div className="AppContainer">
+          <Home />
           <Navbar setShowResponsiveNav={setShowResponsiveNav} showResponsiveNav={showResponsiveNav} />
           <ResponsiveNav setShowResponsiveNav={setShowResponsiveNav} showResponsiveNav={showResponsiveNav} />
-          <Routes>
+          {/* <Routes>
             <Route exact path='/' element={<Home />} />
             <Route exact path='/about' element={<About />} />
             <Route exact path='/solutions' element={<Solution />} />
             <Route exact path='/contact' element={<Contact />} />
             <Route exact path='/blog' element={<Blog />} />
-          </Routes>
+          </Routes> */}
           <Footer />
         </div>
-      </Router>
+      // </Router>
   );
 }
 
