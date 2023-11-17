dc8804cc7ccd975f5a008e3018596003625a3c33
rekha-credencesoft <108658903+rekha-credencesoft@users.noreply.github.com>
2023-11-17 13:23:26 +0530

    Responsive issues fixed
    Fixed for Home page, Solutions page, Case studies, and Blog

--- src/components/SeerInsights/index.js
+++ src/components/SeerInsights/index.js
@@ -8,18 +8,17 @@ const SeerInsights = () => {
   return (
     <div className="SeerInsightsContainer">
       <div>
-        <span className="lg:text-xl text-lg text-white p-5">
-          <b>
+        <p className="lg:text-xl text-sm text-white lg:p-5 md:p-5 p-1 font-bold ">
+         
             POWERED BY PRESCIENT, ACCURATE A.I. ALGORITHMS. Swathes of
             transactions are analysed at light-speed, along with parameters like
             promotions, door counts and more. Powerful, predictive formulae are
             automatically derived to drive your decisions for every SKU, at
-            every store.{" "}
-          </b>
-        </span>
+            every store.
+        </p>
         <div className="videoSectionContainer bg-transparent">
-          <div className="videoSectionLeftSection">
-            <p className="lg:text-lg text-sm">
+          <div className="videoSectionLeftSection ">
+            <p className="lg:text-lg text-sm -mt-16 lg:mt-0 md:mt-0">
               <span className="lg:text-lg text-sm">
                 Stores Sale and Sell-Through Analysis:
               </span>{" "}
@@ -30,13 +29,13 @@ const SeerInsights = () => {
               AnalysisStores Sale and Margin Analysis.
             </p>
           </div>
-          <div className="videoSectionRightSection">
+          <div className="videoSectionRightSection -mt-10 lg:mt-0 md:mt-0">
             <img src={HeroImg} alt="casestudies" class="glassons" />
           </div>
         </div>
         <div className="videoSectionContainer bg-transparent">
-          <div className="videoSectionLeftSection">
-            <p className="lg:text-lg text-sm">
+          <div className="videoSectionLeftSection -mt-10 lg:mt-0 md:mt-0">
+            <p className="lg:text-lg text-sm -mt-16 lg:mt-0 md:mt-0">
               <span className="lg:text-lg text-sm">
                 Sales Person Performance:
               </span>{" "}
@@ -47,13 +46,13 @@ const SeerInsights = () => {
               AnalysisStores Sale and Margin Analysis.
             </p>
           </div>
-          <div className="videoSectionRightSection">
+          <div className="videoSectionRightSection -mt-10 lg:mt-0 md:mt-0">
             <img src={HeroImg} alt="casestudies" class="glassons" />
           </div>
         </div>
         <div className="videoSectionContainer bg-transparent">
-          <div className="videoSectionLeftSection">
-            <p className="lg:text-lg text-sm">
+          <div className="videoSectionLeftSection -mt-10 lg:mt-0 md:mt-0">
+            <p className="lg:text-lg text-sm -mt-16 lg:mt-0 md:mt-0">
               <span className="lg:text-lg text-sm">
                 Vendor Performance:
               </span>{" "}
@@ -64,13 +63,13 @@ const SeerInsights = () => {
               AnalysisStores Sale and Margin Analysis.
             </p>
           </div>
-          <div className="videoSectionRightSection">
+          <div className="videoSectionRightSection -mt-10 lg:mt-0 md:mt-0">
             <img src={HeroImg} alt="casestudies" class="glassons" />
           </div>
         </div>
         <div className="videoSectionContainer bg-transparent">
-          <div className="videoSectionLeftSection">
-            <p className="lg:text-lg text-sm">
+          <div className="videoSectionLeftSection -mt-10 lg:mt-0 md:mt-0">
+            <p className="lg:text-lg text-sm -mt-16 lg:mt-0 md:mt-0">
               <span className="lg:text-lg text-sm">
                 Category and Item Performance Sales and Margin Analysis:
               </span>{" "}
@@ -81,59 +80,59 @@ const SeerInsights = () => {
               AnalysisStores Sale and Margin Analysis.
             </p>
           </div>
-          <div className="videoSectionRightSection">
+          <div className="videoSectionRightSection -mt-10 lg:mt-0 md:mt-0">
             <img src={HeroImg} alt="casestudies" class="glassons" />
           </div>
         </div>
         <div className="videoSectionContainer bg-transparent">
-          <div className="videoSectionLeftSection">
-            <p className="lg:text-lg text-sm">
+          <div className="videoSectionLeftSection -mt-10 lg:mt-0 md:mt-0">
+            <p className="lg:text-lg text-sm -mt-16 lg:mt-0 md:mt-0">
               <span className="lg:text-lg text-sm">
                 Margin Analysis:
               </span>{" "}
               Margin AnalysisMargin AnalysisMargin AnalysisMargin AnalysisMargin AnalysisMargin Analysis Margin AnalysisMargin AnalysisMargin AnalysisMargin Analysis
             </p>
           </div>
-          <div className="videoSectionRightSection">
+          <div className="videoSectionRightSection -mt-10 lg:mt-0 md:mt-0">
             <img src={HeroImg} alt="casestudies" class="glassons" />
           </div>
         </div>
         <div className="videoSectionContainer bg-transparent">
-          <div className="videoSectionLeftSection">
-            <p className="lg:text-lg text-sm">
+          <div className="videoSectionLeftSection -mt-10 lg:mt-0 md:mt-0">
+            <p className="lg:text-lg text-sm -mt-16 lg:mt-0 md:mt-0">
               <span className="lg:text-lg text-sm">
                 Sell-Through Analysis:
               </span>{" "}
               Sell-Through AnalysisSell-Through AnalysisSell-Through AnalysisSell-Through AnalysisSell-Through AnalysisSell-Through Analysis Sell-Through AnalysisSell-Through AnalysisSell-Through AnalysisSell-Through Analysis
             </p>
           </div>
-          <div className="videoSectionRightSection">
+          <div className="videoSectionRightSection -mt-10 lg:mt-0 md:mt-0">
             <img src={HeroImg} alt="casestudies" class="glassons" />
           </div>
         </div>
         <div className="videoSectionContainer bg-transparent">
-          <div className="videoSectionLeftSection">
-            <p className="lg:text-lg text-sm">
+          <div className="videoSectionLeftSection -mt-10 lg:mt-0 md:mt-0">
+            <p className="lg:text-lg text-sm  -mt-16 lg:mt-0 md:mt-0">
               <span className="lg:text-lg text-sm">
               Stock Aging:
               </span>{" "}
               Sell-Through AnalysisSell-Through AnalysisSell-Through AnalysisSell-Through AnalysisSell-Through AnalysisSell-Through Analysis Sell-Through AnalysisSell-Through AnalysisSell-Through AnalysisSell-Through Analysis
             </p>
           </div>
-          <div className="videoSectionRightSection">
+          <div className="videoSectionRightSection -mt-10 lg:mt-0 md:mt-0">
             <img src={HeroImg} alt="casestudies" class="glassons" />
           </div>
         </div>
         <div className="videoSectionContainer bg-transparent">
-          <div className="videoSectionLeftSection">
-            <p className="lg:text-lg text-sm">
+          <div className="videoSectionLeftSection -mt-10 lg:mt-0 md:mt-0">
+            <p className="lg:text-lg text-sm  -mt-16 lg:mt-0 md:mt-0">
               <span className="lg:text-lg text-sm">
               Customer Analysis:
               </span>{" "}
               Sell-Through AnalysisSell-Through AnalysisSell-Through AnalysisSell-Through AnalysisSell-Through AnalysisSell-Through Analysis Sell-Through AnalysisSell-Through AnalysisSell-Through AnalysisSell-Through Analysis
             </p>
           </div>
-          <div className="videoSectionRightSection">
+          <div className="videoSectionRightSection -mt-10 lg:mt-0 md:mt-0">
             <img src={HeroImg} alt="casestudies" class="glassons" />
           </div>
         </div>
