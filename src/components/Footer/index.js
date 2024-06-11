ebc310a83eae4d9db41a3364c94b9fa45d2d8d40
renuka-credencesoft <renuka.adwe@credencesoft.in>
2024-06-11 12:39:57 +0530

    fontsizechange footer changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,31 +1,32 @@
 import React from "react";
 import "./index.css";
-import { Link } from 'react-router-dom';
+import { Link ,useLocation} from 'react-router-dom';
 import { RiMenu2Fill } from 'react-icons/ri';
 import { ImCross } from 'react-icons/im';
 
 const Footer = ({ setShowResponsiveNav, showResponsiveNav, responsiveNav }) => {
+  const location = useLocation();
   return (
     <div className="FooterContainer bottom-0 left-0 right-0 absolute">
       <span style={{ fontFamily:'Comfortaa, sans-serif' }}>©2022 Seer Inc</span>
       <ul className="NavbarLinks">
         <li className="NavbarLink">
-          <Link to="/" className='NavbarLinkText'>Home</Link>
+        <Link to="/" className={location.pathname === '/' ? 'NavbarLinkText active' : 'NavbarLinkText'}>Home</Link>
         </li>
         <li className="NavbarLink">
-          <Link to="/solutions" className='NavbarLinkText'>Solutions</Link>
+        <Link to="/solutions" className={location.pathname === '/solutions' ? 'NavbarLinkText active' : 'NavbarLinkText'}>Solutions</Link>
         </li>
         <li className="NavbarLink">
-          <Link to="/caseStudies" className='NavbarLinkText'>Case Studies</Link>
+        <Link to="/caseStudies" className={location.pathname === '/caseStudies' ? 'NavbarLinkText active' : 'NavbarLinkText'}>Case Studies</Link>
         </li>
         <li className="NavbarLink">
-          <Link to="/about" className='NavbarLinkText'>About Us</Link>
+        <Link to="/about" className={location.pathname === '/about' ? 'NavbarLinkText active' : 'NavbarLinkText'}>About Us</Link>
         </li>
         <li className="NavbarLink">
-          <Link to="/blog" className='NavbarLinkText'>Blog</Link>
+        <Link to="/blog" className={location.pathname === '/blog' ? 'NavbarLinkText active' : 'NavbarLinkText'}>Blog</Link>
         </li>
         <li className="NavbarLink">
-          <Link to="/contact" className='NavbarLinkText'>Contact</Link>
+        <Link to="/contact" className={location.pathname === '/contact' ? 'NavbarLinkText active' : 'NavbarLinkText'}>Contact</Link>
         </li>
       </ul>
       
