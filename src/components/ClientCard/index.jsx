8d1e03f641fcc7f814bdf70fcdd8c60a32e2e90f
rekha-credencesoft <108658903+rekha-credencesoft@users.noreply.github.com>
2024-06-11 18:38:04 +0530

    Fixed the size of carousal text

--- src/components/ClientCard/index.jsx
+++ src/components/ClientCard/index.jsx
@@ -13,13 +13,13 @@ const ClientCard = () => {
 
       <div className="ClientCardContent mt-8  ">
         
-        <p style={{fontFamily:'Roboto, sans-serif'}}className="text-left text-[15px]  ">
+        <p style={{fontFamily:'Roboto, sans-serif'}}className="text-left  ">
          " Out teams are spending days a week managing the parameters which drive
           our vendor re-order calculation. Seer AI Forecasts will help us
           automate the process, eliminate manual effort, and improve order
           accuracy."
         </p>
-        <h3 className="text-center  text-[15px] lg:text-[15px] md:text-[15px]"  style={{fontFamily:'gothic-book, sans-serif'}}>
+        <h3 className="text-center"  style={{fontFamily:'gothic-book, sans-serif'}}>
           <span style={{fontFamily:"roboto"}}>-</span>
          
           PHILLIP CHRISTOPHER, C.I.O., ANIMATES NZ
