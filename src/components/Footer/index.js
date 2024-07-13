3614133457ad6bdcc020b7b7b80c67af10513cd6
Rekha-Credencesoft <biswajit.sahoo@credencesoft.in>
2024-07-13 23:47:51 +0530

    footer-responsive

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,16 +3,38 @@ import "./index.css";
 import { Link, useLocation } from "react-router-dom";
 import { RiMenu2Fill } from "react-icons/ri";
 import { ImCross } from "react-icons/im";
-
+import logo from "../../assets/images/logo-credencesoft.png";
 const Footer = ({ setShowResponsiveNav, showResponsiveNav, responsiveNav }) => {
   const location = useLocation();
   return (
     <div className="FooterContainer bottom-0 left-0 right-0 absolute">
       <span style={{ fontFamily: "gothic-book" }}>
         ©202<span style={{ fontFamily: "serif" }}>4</span> Seer Inc <br />
-        Made with ❤️ by CredenceSoft
-      </span>{" "}
-      <ul className="NavbarLinks">
+        <span
+          style={{
+            display: "flex",
+            alignItems: "center",
+            flexWrap: "nowrap",
+            whiteSpace: "nowrap",
+          }}
+        >
+          <img
+            src={logo}
+            alt="Small Image"
+            style={{
+              width: "22px",
+              height: "14px",
+              verticalAlign: "middle",
+              marginRight: "5px",
+              marginTop: "-2px",
+              // borderRadius: "50%",
+            }}
+          />
+          Made with ❤️ by CredenceSoft
+        </span>
+      </span>
+
+      <ul className="NavbarLinks pt-4">
         <li className="NavbarLink">
           <Link
             to="/"
